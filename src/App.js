f6960c6cf05920751e1ae7f88deba7164192bcac
Matt Ferlo <mferlo@us.imshealth.com>
2017-10-16 13:31:59 -0700

    General refactor. Logic appears to work

--- src/App.js
+++ src/App.js
@@ -1,105 +1,115 @@
 import React, { Component } from 'react';
 
 // TODO:
-//  * write "assigned" logic, by faking it with a hard-coded button
-//  * write "unassign" logic, ditto
-//  Then, give it a UI
+//  Give it a UI
 //  * make click handler for the trigrams, which makes them "activated"
 //  * make click handler for the pattern, which will consume the "activated"
 //  * make click handler for trigrams that are in the pattern
 //  * do something with graphics to indicate state & valid drop zones
 //  Then, iterate on it
-//  * less shitty input method
-//  * word suggest (literally all combos, and dictionary)
+//  * way to input the puzzle data on the page itself
+//  * word suggest (literally all combos, then dictionary)
+//    * need to make pattern aware of the concept of a word
 //  * drag and drop
 
-class T {
-  // "pattern" is array of strings. If string is "",
-  // it is a placeholder, otherwise a literal.
-  // "trigrams" is array of strings.
-  static create(pattern, trigrams) {
-    let p = [];
-    let trigramIndex = 0; // [0, 0, 0, 1, 1, 1, 2, ...]
-    let trigramCount = 0;
-    for (let i = 0; i <= pattern.length; i++) {
-      if (pattern[i]) {
-        p[i] = { isLiteral: true, value: pattern[i] };
+class Trigram {
+  constructor(letters) {
+    this.letters = letters;
+    this.assignedTo = null;
+  }
+}
+
+class PatternTrigram {
+  constructor(pattern) {
+    this.pattern = pattern;
+    this.assignedTrigram = null;
+  }
+
+  getText() {
+    let result = "";
+    let replacementLetters = this.assignedTrigram || "???";
+    let replacementIndex = 0;
+
+    for (let i = 0; i < this.pattern.length; i++) {
+      if (this.pattern[i] === "") {
+        result += replacementLetters.charAt(replacementIndex);
+        replacementIndex++;
       } else {
-        p[i] = { isLiteral: false,
-                 trigramIndex,
-                 internalTrigramIndex: trigramCount };
-        trigramCount += 1;
-        if (trigramCount === 3) {
-          trigramIndex += 1;
-          trigramCount = 0;
-        }
+        result += this.pattern[i];
       }
     }
+    return result;
+  }
+}
 
+class Pattern {
+  constructor(pattern) { // Is expected to be an array of strings
+    let patternTrigrams = [];
+
+    // TODO: Make each letter of PatternTrigram aware of which word it belongs to
+    let startIndex = 0;
+
+    do {
+      let endIndex = startIndex;
+      let placeholderCount = 0;
+
+      while (placeholderCount < 3 && endIndex < pattern.length) {
+        if (!pattern[endIndex]) { // It's a placeholder
+          placeholderCount += 1;
+        }
+        endIndex += 1;
+      }
+
+      patternTrigrams.push(new PatternTrigram(pattern.slice(startIndex, endIndex)));
+
+      startIndex = endIndex;
+    } while (startIndex < pattern.length)
+
+    this.patternTrigrams = patternTrigrams;
+  }
+}
+
+
+class T {
+  static create(pattern, trigrams) {
     return {
-      pattern: p,
-      trigrams: trigrams.map(
-        (t, i) => { return { value: t, index: i, mappedTo: null }; }),
+      pattern: new Pattern(pattern),
+      trigrams: trigrams.map(t => new Trigram(t))
     };
   }
 
-  static getMappings(state) {
-    const result = [];
-    state.trigrams.forEach(
-      t => { if (t.mappedTo !== null) { result[t.mappedTo] = t.value; } });
-    return result;
-  }
-
-  static maybeGetMappedChar(state, trigramIndex, internalTrigramIndex) {
-    
-  }
-
-  static assign(state, trigram, patternIndex) {
-    if (trigram.assigned) {
+  static assign(state, trigramText, i) {
+    const trigramToAssign = state.trigrams.findIndex(t => t.letters === trigramText && t.assignedTo === null);
+    if (trigramToAssign === -1) {
       return state;
     }
 
-    
+    state.trigrams[trigramToAssign].assignedTo = i;
+    state.pattern.patternTrigrams[i].assignedTrigram = trigramText;
+
+    return state;
   }
 
-  static getText(state) {
-    let text = "";
-    let i = 0;
-    let ii = 0;
-    for (let p of state.pattern) {
-      if (p.isLiteral) {
-        text += p.value;
-      } else {
-        text += T.maybeGetMappedChar(state, i, ii);
-        ii += 1;
-        if (ii === 3) {
-          ii = 0;
-          i += 1;
-        }
-      }
+  static unassign(state, i) {
+    const trigramToUnassign = state.trigrams.findIndex(t => t.assignedTo === i);
+    if (trigramToUnassign === -1) {
+      return state;
     }
-    return text;
+
+    state.trigrams[trigramToUnassign].assignedTo = null;
+    state.pattern.patternTrigrams[i].assignedTrigram = null;
+
+    return state;
   }
 }
 
-class Trigram extends Component {
+class TrigramDisplay extends Component {
   render() {
     const className = this.props.assigned ? " assigned" : "";
     return (
-      <span className={"trigram " + className}>
-        {this.props.value}
+      <span className={"trigram" + className}>
+        {this.props.letters}
       </span>);
-  }
-}
-
-class Trigrams extends Component {
-
-  render() {
-    const makeKey = t => `${t.index}-${t.value}-${t.assigned}`;
-    return (
-      <div className="trigrams">
-        {this.props.trigrams.map(t => <Trigram key={makeKey(t)} {...t} />)}
-      </div>);
   }
 }
 
@@ -111,16 +121,44 @@ class App extends Component {
       ["", "", "", "'", "", " ", "", ""],
       [ "DGO", "YOU" ]
     );
+
+    console.log(this.state);
+  }
+
+  renderTrigram(t) {
+    const key = `${t.letters}-${t.assignedTo}`;
+    return <TrigramDisplay key={key} letters={t.letters} assigned={t.assignedTo !== null} />;
+  }
+
+  renderTrigrams() {
+    return (<div>
+      {this.state.trigrams.map(t => this.renderTrigram(t))}
+    </div>);
+  }
+
+  renderPattern() {
+    return this.state.pattern.patternTrigrams.map(
+      (t, i) => <span key={t+i} className={"pattern " + i}>{t.getText()}</span>);
+  }
+
+  assign() {
+    this.setState(oldState => T.assign(oldState, "YOU", 0));
+  }
+
+  unassign() {
+    this.setState(oldState => T.unassign(oldState, 0));
   }
 
   render() {
-    const text = T.getText(this.state);
 
     return (
       <div>
-        <Trigrams trigrams={this.state.trigrams} />
+        {this.renderTrigrams()}
         <br />
-        {text}
+        {this.renderPattern()}
+        <br />
+        <button style={{width:"100px", height:"50px"}} onClick={e => this.assign()} />
+        <button style={{width:"100px", height:"50px"}} onClick={e => this.unassign()} />
       </div>
     );
   }
